472d0c1d59816f297751c4f1fab15afe58521e1f
grozamorei <desperatebassplayer@gmail.com>
2018-05-01 09:30:26 +0300

    temporarily removed ui framework

--- gulpfile.js
+++ gulpfile.js
@@ -94,11 +94,12 @@ gulp.task('finish-deploy', ['step2-webpack'], () => {
     if (process.env.MODE === 'development') {
         const sourcemaps = require('gulp-sourcemaps')
         return gulp.src([
-            'src/lib/fullscreen-api-polyfill.lib.js',
-            'src/lib/stats.lib.js',
-            'src/lib/tweenjs.lib.js',
-            'src/lib/pixi.lib.js',
-            'src/lib/fairygui.lib.js',
+            'src/lib/*.lib.js'
+            // 'src/lib/fullscreen-api-polyfill.lib.js',
+            // 'src/lib/stats.lib.js',
+            // 'src/lib/tweenjs.lib.js',
+            // 'src/lib/pixi.lib.js',
+            // 'src/lib/fairygui.lib.js',
         ])
             .pipe(sourcemaps.init())
             .pipe(concat('libraries.js'))
@@ -106,10 +107,11 @@ gulp.task('finish-deploy', ['step2-webpack'], () => {
             .pipe(gulp.dest('build'))
     } else {
         return gulp.src([
-            'src/lib/fullscreen-api-polyfill.lib.min.js',
-            'src/lib/tweenjs.lib.min.js',
-            'src/lib/pixi.lib.min.js',
-            'src/lib/fairygui.lib.min.js'
+            'src/lib/*.lib.min.js'
+            // 'src/lib/fullscreen-api-polyfill.lib.min.js',
+            // 'src/lib/tweenjs.lib.min.js',
+            // 'src/lib/pixi.lib.min.js',
+            // 'src/lib/fairygui.lib.min.js'
         ])
             .pipe(concat('libraries.js'))
             .pipe(gulp.dest('build'))
