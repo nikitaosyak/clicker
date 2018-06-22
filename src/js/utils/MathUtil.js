08adabdfadbb14de202f87b5fc3c4367ab254e03
grozamorei <desperatebassplayer@gmail.com>
2018-06-22 16:07:44 +0300

    fixed undefined gold counter

--- src/js/utils/MathUtil.js
+++ src/js/utils/MathUtil.js
@@ -5,6 +5,8 @@ export const MathUtil  = {
         return Math.round(value * Math.pow(10, digit)) / Math.pow(10, digit)
     },
     convert: value => {
+        if (Math.abs(value) < 0.00001) return '0'
+
         const pow = Math.floor(Math.log10(value)/3)
         const letter = letters[pow]
         return `${MathUtil.roundToDigit(value/Math.pow(10, pow*3), 2)}${letter}`
