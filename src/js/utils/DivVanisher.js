1a7c6675812ca8d1871a391870515cf48a53eb5e
grozamorei <desperatebassplayer@gmail.com>
2018-09-04 17:40:55 +0300

    added wait before loader disappearance

--- src/js/utils/DivVanisher.js
+++ src/js/utils/DivVanisher.js
@@ -3,13 +3,20 @@ export const DivVanisher = () => {
 
     const preloaderImage = document.getElementById('loader')
     const progressBar = document.getElementById('progress')
+    const WAIT_INSTANTIATE = 0.5
     const VANISH_SPEED = 1.3
+    let currentTimer = 0
     let currentOpacity = 1
     let done = false
 
     return {
         get running() { return !done },
         update: dt => {
+            if (currentTimer < WAIT_INSTANTIATE) {
+                currentTimer += dt
+                return
+            }
+
             currentOpacity -= VANISH_SPEED * dt
             if (currentOpacity <= 0) {
                 done = true
